dbb6f97d589bc3d93e37d02e60e29b00a1d99e8d
anatomicalfrog <sgmoore@princeton.edu>
2016-11-13 02:18:39 -0500

    Delete recipe.py

--- recipe.py
+++
@@ -1,8 +0,0 @@
-class Recipe(object):
-    def __init__(self,meal,category,title): #initial
-        self.meal = meal
-        self.category = category
-        self.title = title
-
-    def __str__(self): #string method
-        return self.title
